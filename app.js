a2cc16ac218027d332e0be3f6157fb20b28c1f6f
Akash Shekara <akashshekara1212@gmail.com>
2016-10-22 15:56:15 -0700

    One board working

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var five = require("johnny-five")
+  , board = new five.Board();
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,6 +20,10 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(function(req, res, next){
+  res.io = io;
+  next();
+});
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -56,5 +64,49 @@ app.use(function(err, req, res, next) {
   });
 });
 
+board.on("ready", function() {
+    // Get the imu data from the board here
+    var imu = new five.IMU({
+      controller: "BNO055"
+    });
 
-module.exports = app;
+    imu.orientation.on("change", function() {
+      console.log("orientation");
+      console.log("  w            : ", this.quarternion.w);
+      console.log("  x            : ", this.quarternion.x);
+      console.log("  y            : ", this.quarternion.y);
+      console.log("  z            : ", this.quarternion.z);
+
+      console.log("  heading      : ", this.euler.heading);
+      console.log("  roll         : ", this.euler.roll);
+      console.log("  pitch        : ", this.euler.pitch);
+
+      console.log("  acc x        : ", this.euler.heading);
+      console.log("  acc y        : ", this.euler.roll);
+      console.log("  acc z        : ", this.euler.pitch);
+
+      console.log("--------------------------------------");
+      yaw = this.euler.heading;
+      roll = this.euler.roll;
+      pitch = this.euler.pitch;
+      
+      // console.log(board.Accelerometer.board);
+
+      io.emit("socketToMe", {yaw: yaw,roll: roll,pitch: pitch});
+    });
+
+    // this.i2cConfig(address: );
+    // this.i2cRead(0x0A, 0, function(data) {
+    //   console.log("  ✔ received data (exiting)");
+    //   console.log("------------------------------");
+    //   console.log(data);
+    // });
+    // new five.IMU({
+    //   controller: "BNO055",
+    //   address: 0x68, // optional
+    //   freq: 100      // optional
+    // });
+});
+
+
+module.exports = {app: app, server: server};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var THREE = require('three');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+// io.on("connection",function(socket){
+//   console.log("yaw: " + yaw + ", roll: " + roll + ", pitch" + pitch);
+//   socket.emit('orientation', {yaw: yaw,roll: roll,pitch: pitch});
+// });
 
 module.exports = router;
